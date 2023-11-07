a54fb79470c53dca10580578427b3dd319e4f541
Sycinz <gronbosof@gmail.com>
2023-11-07 17:06:04 +0100

    Added saving form data in session storage

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,17 +1,29 @@
 function Form(props) {
     const {formData, setFormData} = props
 
+    // Saving login data in sessionStorage
+    Object.keys(formData).map(v => {
+        sessionStorage.setItem(formData[v], v)
+    })
+
+    // handling subnmit <button>
     const handleSubmit = (event) => {
         const {name, value} = event.target
     }
 
+    // handler that changes formData object value on every change
     const handleChange = (event) => {
         const {id, value} = event.target
 
+    // setting previous formData state + overwriting new values 
         setFormData(prevData => ({
             ...prevData,
             [id]: value
         }))
+
+        Object.keys(formData).map(v => {
+            console.log(`sessionStorage values [${v}]: ${sessionStorage.getItem(v)}`)
+        })
     }
 
     console.log(formData)
@@ -19,9 +31,9 @@ function Form(props) {
     return (
         <form name="form" onSubmit={handleSubmit}>
             <div className="inputs">
-                <input id="user" onChange={handleChange} type="text" value={formData.user}/>
-                <input id="psswd" onChange={handleChange} type="password" value={formData.psswd}/>
-                <input id="psswdRepeat" onChange={handleChange} type="password" value={formData.psswdRepeat}/>
+                <input id="user" onChange={handleChange} type="text" value={formData.user} />
+                <input id="psswd" onChange={handleChange} type="password" value={formData.psswd} />
+                <input id="psswdRepeat" onChange={handleChange} type="password" value={formData.psswdRepeat} />
             </div>
             <button>Login</button>
         </form>
